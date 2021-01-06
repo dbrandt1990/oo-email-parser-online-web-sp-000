f7fa41febd0ce0128cdadf9880c2f126cefbcf3a
Danny Brandt <dvbrandt90@gmail.com>
2021-01-06 22:37:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,6 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser(email) 
-  
+class EmailAddressParser(string)
+  def parse
+  end
 end
